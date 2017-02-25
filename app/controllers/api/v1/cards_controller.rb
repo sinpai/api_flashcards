aec116dddf4b1f2205d4d1cc71e1839b33879447
sinpai <monaco90@gmail.com>
2017-02-25 20:23:41 +0200

    gix

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,32 +1,36 @@
 require_relative '../api_controller.rb'
 
 module ApiFlashcards
-  class Api::V1::CardsController < ApiFlashcards::Api::ApiController
+  module Api
+    module V1
+      class CardsController < ApiFlashcards::Api::ApiController
 
-    respond_to :json
+        respond_to :json
 
-    def index
-      respond_with Card.all
-    end
+        def index
+          respond_with Card.all
+        end
 
-    def show
-      respond_with Card.find(params[:id])
-    end
+        def show
+          respond_with Card.find(params[:id])
+        end
 
-    def create
-      card = current_user.cards.build(card_params)
-      if card.save
-        render json: card, status: 201, location: [:api, card]
-      else
-        render json: { errors: card.errors }, status: 422
+        def create
+          card = current_user.cards.build(card_params)
+          if card.save
+            render json: card, status: 201, location: [:api, card]
+          else
+            render json: { errors: card.errors }, status: 422
+          end
+        end
+
+        private
+
+        def card_params
+          params.require(:card).permit(:original_text, :translated_text, :review_date,
+                                       :image, :image_cache, :remove_image, :block_id, :user_id, :remote_image_url)
+        end
       end
-    end
-
-    private
-
-    def card_params
-      params.require(:card).permit(:original_text, :translated_text, :review_date,
-                                   :image, :image_cache, :remove_image, :block_id, :user_id, :remote_image_url)
     end
   end
 end
--- lib/api_flashcards.rb
+++ lib/api_flashcards.rb
@@ -2,8 +2,4 @@ require "api_flashcards/engine"
 
 module ApiFlashcards
   # Your code goes here...
-  module Api
-    module V1
-    end
-  end
 end
