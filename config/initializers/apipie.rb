15c72ca254fb857a43572a7468cf2d76eaccf2f3
Alexander Morozov <alexander.morozov@postindustria.com>
2017-02-26 16:35:34 +0200

    fix

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,7 @@
 Apipie.configure do |config|
-  config.app_name                = "NilClass"
+  config.app_name                = "ApiFlashcards"
   config.api_base_url            = "/api"
-  config.doc_base_url            = "/apipie"
+  config.doc_base_url            = "/doc"
   # where is your API defined?
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.api_controllers_matcher = "#{ApiFlashcards::Engine.root}/app/controllers/**/*.rb"
 end
